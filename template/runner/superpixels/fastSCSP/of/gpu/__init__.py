7f35951e359d13538e97c1f6632e5a0b3b84f800
Renthal <albertimichele91@hotmail.com>
2019-10-03 15:11:12 +0100

    removed and we have no use for it rn

--- template/runner/superpixels/fastSCSP/of/gpu/__init__.py
+++
@@ -1,3 +0,0 @@
-from .CpuGpuArray import CpuGpuArray
-
-__all__ = ['CpuGpuArray']
--- template/runner/superpixels/fastSCSP/superpixels/gpu/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-dirname_of_cuda_files = os.path.abspath(os.path.dirname(str(__file__)))
-
-
-if __name__ == "__main__":
-    print('dirname_of_cuda_files:',dirname_of_cuda_files)
