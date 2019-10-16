983f992bc9a03c9e3be7f8e4fa51d121a6cc5526
Renthal <albertimichele91@hotmail.com>
2019-10-16 13:49:58 +0100

    first test implemented and passes

--- template/test_RunMe.py
+++ template/test_RunMe.py
@@ -11,24 +11,31 @@ the main functions can be tested.
 
 Please keep the list of these tests up to date as soon as you add new features.
 """
-import os
+import shutil
+
 import numpy as np
 import pytest
+from pathlib import Path
 
+from datasets.util.get_a_dataset import cifar10
 from template.RunMe import RunMe
 
-
-INPUT_PATH="/local/scratch/dataset"
-OUTPUT_PATH="/local/scratch/albertim/output"
+INPUT_PATH = Path().absolute() / 'test_data'
+OUTPUT_PATH = Path().absolute() / 'test_output'
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
-    # Code that will run before your test, for example:
-    print("Checking data is there")
-    assert os.path.exists(INPUT_PATH)
+    # Prepare the test dataset
+    print("Preparing data")
+    INPUT_PATH.mkdir(exist_ok=True)
+    OUTPUT_PATH.mkdir(exist_ok=True)
+    cifar10(output_folder=INPUT_PATH)
     # A test function will be run at this point
+    print("Running test")
     yield
-    # Code that will run after your test, for example:
+    # Remove the test dataset and outputs
+    shutil.rmtree(INPUT_PATH)
+    shutil.rmtree(OUTPUT_PATH)
     print("Done!")
 
 def test_one():
@@ -36,31 +43,33 @@ def test_one():
     - Verify the sizes of the return of execute
     - Image classification with default parameters
     """
-    epochs = 1
+    epochs = 2
     args = ["-rc", "ImageClassification",
             "--experiment-name", "test_image_classification",
             "--ignoregit",
-            "--input-folder", os.path.join(INPUT_PATH,"CIFAR10"),
-            "--output-folder", OUTPUT_PATH,
+            "--input-folder", str(INPUT_PATH / "CIFAR10"),
+            "--output-folder", str(OUTPUT_PATH),
             "--model-name", "CNN_basic",
             "--seed", "42",
             "--epochs", str(epochs)]
 
-    train, val, test = RunMe().start(args=args)
+    payload = RunMe().start(args=args)
 
     # Verify sizes of the return values from execute
-    assert len(train.shape) == 2
-    assert train.shape[0] == 1
-    assert train.shape[1] == epochs
+    assert len(payload.keys()) == 3
+    assert 'train' in payload
+    assert 'val' in payload
+    assert 'test' in payload
 
-    assert len(val.shape) == 2
-    assert train.shape[0] == 1
-    assert train.shape[1] == epochs+1
+    assert payload['train'] is not None
+    assert len(payload['train'].shape) == 1
+    assert payload['train'].shape[0] == epochs
 
-    assert len(train.shape) == 1
-    assert train.shape[0] == 1
+    assert payload['val'] is not None
+    assert len(payload['val'].shape) == 1
+    assert payload['val'].shape[0] == epochs + 1
 
-    # Verify values
-    np.testing.assert_almost_equal(train[0], 100.0)
-    np.testing.assert_almost_equal(val[0], 100.0)
-    np.testing.assert_almost_equal(test, 100.0)
+    assert payload['test'] is not None
+    assert isinstance(payload['test'], float)
+
+
