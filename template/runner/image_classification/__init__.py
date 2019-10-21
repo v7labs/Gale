ef56c2712979ed2c73027ad537d3966f205327dd
Michele Alberti <albertimichele91@hotmail.com>
2019-10-21 16:16:04 +0100

    Detection darwin-py integration (#3)
    Now detection works with a dataset downloaded from darwin

--- template/runner/image_classification/__init__.py
+++ template/runner/image_classification/__init__.py
@@ -1,4 +1,5 @@
 from .image_classification import ImageClassification
 from .image_classification_inference import ImageClassificationInference
+from .CL_arguments import CLArguments
 
 __all__ = ['ImageClassification', 'ImageClassificationInference']
