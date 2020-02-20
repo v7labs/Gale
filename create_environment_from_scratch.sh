eadba468680ba1bf8cfa46ededf370297e40edd1
Michele Alberti <albertimichele91@hotmail.com>
2020-02-20 11:57:58 +0100

    Update create_environment_from_scratch.sh

--- create_environment_from_scratch.sh
+++ create_environment_from_scratch.sh
@@ -2,5 +2,4 @@
 
 conda create -y -n gale python=3.7.4 colorlog numpy pandas tqdm matplotlib seaborn pytorch torchvision cudatoolkit=10.1 psutil -c pytorch conda-forge
 source activate gale
-conda install -y pytorch torchvision cudatoolkit=10.1 -c pytorch
 pip install sigopt tensorboardx darwin-py
