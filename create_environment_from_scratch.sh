863f5ab479f1729c4fd312e90856b30c33372e6f
vinaychandranp <vinaychandranp@gmail.com>
2020-02-20 11:36:29 +0100

    bugfix - activate env before installing remaining packages

--- create_environment_from_scratch.sh
+++ create_environment_from_scratch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda create -n gale python=3
-conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
-conda install colorlog numpy pandas tqdm matplotlib seaborn
+conda create -y -n gale python=3 colorlog numpy pandas tqdm matplotlib seaborn
+source activate gale
+conda install -y pytorch torchvision cudatoolkit=10.1 -c pytorch
 pip install sigopt tensorboardx darwin-py
